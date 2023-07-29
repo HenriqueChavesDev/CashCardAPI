a4bc3b0a82e5098dc37786ea6bfa3393cf353f3e
Henrique Chaves <henriquechaves.jobs@gmail.com>
2023-07-29 01:50:53 -0300

    feat(CashCard): set entity from db

--- src/main/java/com/henriquechaves/cashcard/CashCard.java
+++ src/main/java/com/henriquechaves/cashcard/CashCard.java
@@ -1,5 +1,5 @@
 package com.henriquechaves.cashcard;
 
-public record CashCard(Long id, Double amount) {
-    
-}
+import org.springframework.data.annotation.Id;
+
+public record CashCard(@Id Long id, Double amount) {}
