f4d40baf779bc50aa98c993af61921d68aafd4ea
Henrique Chaves <henriquechaves.jobs@gmail.com>
2023-07-31 22:26:18 -0300

    feat(CashCard): add endpoint to return list of cashCards

--- src/main/java/com/henriquechaves/cashcard/CashCardController.java
+++ src/main/java/com/henriquechaves/cashcard/CashCardController.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriComponentsBuilder;
 
 import java.net.URI;
+import java.util.Iterator;
 import java.util.Optional;
 
 @RestController
@@ -32,5 +33,10 @@ public class CashCardController {
         CashCard savedCashCard = cashCardRepository.save(newCashCard);
         URI locationOfNewCashCard = ucb.path("api/v1/cashcards/{id}").buildAndExpand(savedCashCard.id()).toUri();
         return ResponseEntity.created(locationOfNewCashCard).build();
+    }
+
+    @GetMapping
+    public ResponseEntity<Iterable<CashCard>> findAll(){
+        return ResponseEntity.ok(cashCardRepository.findAll());
     }
 }
