6523ebd84b82d5c6ca4f071de82ebd4a63e689d8
Henrique Chaves <henriquechaves.jobs@gmail.com>
2023-08-11 21:55:46 -0300

    feat(CashCard): implement pageable function with default sorting asc

--- src/main/java/com/henriquechaves/cashcard/CashCardController.java
+++ src/main/java/com/henriquechaves/cashcard/CashCardController.java
@@ -3,6 +3,7 @@ package com.henriquechaves.cashcard;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriComponentsBuilder;
@@ -40,7 +41,7 @@ public class CashCardController {
 
     @GetMapping
     public ResponseEntity<List<CashCard>> findAll(Pageable pageable){
-        Page<CashCard> page = cashCardRepository.findAll(PageRequest.of(pageable.getPageNumber(), pageable.getPageSize()));
+        Page<CashCard> page = cashCardRepository.findAll(PageRequest.of(pageable.getPageNumber(), pageable.getPageSize(), pageable.getSortOr(Sort.by(Sort.Direction.ASC, "amount"))));
 
         return ResponseEntity.ok(page.getContent());
     }
