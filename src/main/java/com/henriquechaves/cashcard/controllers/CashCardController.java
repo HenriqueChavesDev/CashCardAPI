db941d42cafb8bb7ecbb99ccab65ba7cfafd260d
Henrique Chaves <henriquechaves.jobs@gmail.com>
2023-08-24 01:17:55 -0300

    feat(save): router to save new cash card

--- src/main/java/com/henriquechaves/cashcard/controllers/CashCardController.java
+++ src/main/java/com/henriquechaves/cashcard/controllers/CashCardController.java
@@ -1,7 +1,8 @@
 package com.henriquechaves.cashcard.controllers;
 
 
-import com.henriquechaves.cashcard.CashCardRepository;
+import com.henriquechaves.cashcard.dtos.CreateCashCardDTO;
+import com.henriquechaves.cashcard.repositories.CashCardRepository;
 import com.henriquechaves.cashcard.entities.CashCard;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
@@ -9,6 +10,9 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
 
 @RestController
 @RequestMapping("/api/v1/cashcards")
@@ -23,7 +27,18 @@ public class CashCardController {
 
     @GetMapping
     public ResponseEntity<Page<CashCard>> listCashCards(Pageable pageable){
-        Page<CashCard> cashCardPage = cashCardRepository.getPaginatedCashCards(PageRequest.of(pageable.getPageNumber(), pageable.getPageSize(), Sort.by(Sort.Direction.ASC, "amount")));
+        Page<CashCard> cashCardPage = cashCardRepository.findAll(PageRequest.of(pageable.getPageNumber(), pageable.getPageSize(), Sort.by(Sort.Direction.ASC, "amount")));
         return ResponseEntity.ok(cashCardPage);
+    }
+
+    @PostMapping
+    private ResponseEntity<Void> createNewCashCard(CreateCashCardDTO data){
+        CashCard cashCard = cashCardRepository.save(new CashCard(data.amount(), data.owner()));
+        URI uri = ServletUriComponentsBuilder
+                .fromCurrentRequestUri()
+                .path("/{id}")
+                .buildAndExpand(cashCard.getId())
+                .toUri();
+        return ResponseEntity.created(uri).build();
     }
 }
