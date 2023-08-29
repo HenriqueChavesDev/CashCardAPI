d861caea7bcff7a5988a03acf4875f647623a4e7
Henrique Chaves <henriquechaves.jobs@gmail.com>
2023-08-28 22:39:11 -0300

    test: add integration tests use mockMvc

--- src/test/java/com/henriquechaves/cashcard/controllers/integration/CashCardControllerIntegrationTests.java
+++ src/test/java/com/henriquechaves/cashcard/controllers/integration/CashCardControllerIntegrationTests.java
@@ -1,30 +1,31 @@
-package com.henriquechaves.cashcard.controllers.unity;
+package com.henriquechaves.cashcard.controllers.integration;
 
+import com.henriquechaves.cashcard.controllers.CashCardController;
 import com.henriquechaves.cashcard.dtos.CreateCashCardDTO;
-import com.henriquechaves.cashcard.repositories.CashCardRepository;
 import com.henriquechaves.cashcard.entities.CashCard;
+import com.henriquechaves.cashcard.repositories.CashCardRepository;
 import com.henriquechaves.cashcard.utils.CashCardTestHelper;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.ResultMatcher;
 
 import java.util.Optional;
+
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.*;
 
 import static org.mockito.BDDMockito.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
-@WebMvcTest
-@ExtendWith(MockitoExtension.class)
-public class CashCardControllerUnityTests {
+@WebMvcTest(CashCardController.class)
+@WithMockUser(username = "henrique", roles = {"USER", "ADMIN"})
+public class CashCardControllerIntegrationTests {
 
     @Autowired
     private MockMvc mockMvc;
@@ -34,9 +35,9 @@ public class CashCardControllerUnityTests {
 
     @Test
     void shouldBeReturnListOfCashCardsWithSuccess() throws Exception {
-        var cashCardPage = new PageImpl<CashCard>(CashCardTestHelper.list, PageRequest.of(0,10), CashCardTestHelper.list.size());
+        var cashCardPage = new PageImpl<CashCard>(CashCardTestHelper.list, PageRequest.of(0, 10), CashCardTestHelper.list.size());
         given(cashCardRepository.findAll(any(PageRequest.class))).willReturn(cashCardPage);
-        mockMvc.perform(get("/api/v1/cashcards"))
+        mockMvc.perform(get("/api/v1/cashcards").with(httpBasic("henrique", "1234")))
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$.content[0].id").value(1L))
                 .andExpect(jsonPath("$.content[1].id").value(2L))
@@ -50,8 +51,10 @@ public class CashCardControllerUnityTests {
 
         given(cashCardRepository.save(any(CashCard.class))).willReturn(result);
         mockMvc.perform(post("/api/v1/cashcards")
-                .accept(MediaType.APPLICATION_JSON)
-                .contentType(MediaType.APPLICATION_JSON)
+                        .with(httpBasic("henrique", "1234"))
+                        .with(csrf())
+                        .accept(MediaType.APPLICATION_JSON)
+                        .contentType(MediaType.APPLICATION_JSON)
                         .content(CashCardTestHelper.parseJsonTo(newCashCard)))
                 .andExpect(status().isCreated())
                 .andExpect(header().exists("Location"))
@@ -62,32 +65,34 @@ public class CashCardControllerUnityTests {
     void shouldBeReturnOnlyOneCashCardWhenReceiveID() throws Exception {
         var result = new CashCard(7L, 120.88, "henrique");
         given(cashCardRepository.findById(anyLong())).willReturn(Optional.of(result));
-        mockMvc.perform(get("/api/v1/cashcards/7"))
+        mockMvc.perform(get("/api/v1/cashcards/7").with(httpBasic("henrique", "1234"))
+                        .with(csrf()))
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$.id").exists())
                 .andExpect(jsonPath("$.id").value(7L));
     }
-    @Test
-     void shouldResponseNotFoundWhenReceiveIdCashCardNotExist() throws Exception {
-        given(cashCardRepository.findById(222L)).willReturn(Optional.empty());
-        mockMvc.perform(get("/api/v1/cashcards/222"))
-                .andExpect(status().isNotFound());
-     }
 
-     @Test
+    @Test
+    void shouldResponseNotFoundWhenReceiveIdCashCardNotExist() throws Exception {
+        given(cashCardRepository.findById(222L)).willReturn(Optional.empty());
+        mockMvc.perform(get("/api/v1/cashcards/222").with(httpBasic("henrique", "1234")).with(csrf()))
+                .andExpect(status().isNotFound());
+    }
+
+    @Test
     void shouldDeleteCashCardByIdIsSuccess() throws Exception {
-         given(cashCardRepository.existsById(1L)).willReturn(true);
-         mockMvc.perform(delete("/api/v1/cashcards/1"))
+        given(cashCardRepository.existsById(1L)).willReturn(true);
+        mockMvc.perform(delete("/api/v1/cashcards/1").with(httpBasic("henrique", "1234")).with(csrf()))
                 .andExpect(status().isNoContent());
 
-         verify(cashCardRepository).existsById(1L);
-         verify(cashCardRepository).deleteById(1L);
+        verify(cashCardRepository).existsById(1L);
+        verify(cashCardRepository).deleteById(1L);
     }
 
     @Test
     void shouldDeleteCashCardByIdIsFail() throws Exception {
         given(cashCardRepository.existsById(178L)).willReturn(false);
-        mockMvc.perform(delete("/api/v1/cashcards/178"))
+        mockMvc.perform(delete("/api/v1/cashcards/178").with(httpBasic("henrique", "1234")).with(csrf()))
                 .andExpect(status().isNotFound());
 
         verify(cashCardRepository).existsById(178L);
@@ -96,11 +101,13 @@ public class CashCardControllerUnityTests {
 
     @Test
     void shouldBeUpdatedCashCardWithSuccess() throws Exception {
-        var card = new CashCard(23L,239.99, "henrique");
-        var cardResult = new CashCard(23L,239.99, "henrique");
+        var card = new CashCard(23L, 239.99, "henrique");
+        var cardResult = new CashCard(23L, 239.99, "henrique");
         given(cashCardRepository.existsById(23L)).willReturn(true);
         given(cashCardRepository.save(card)).willReturn(cardResult);
         mockMvc.perform(put("/api/v1/cashcards/23")
+                        .with(httpBasic("henrique", "1234"))
+                        .with(csrf())
                         .accept(MediaType.APPLICATION_JSON)
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(CashCardTestHelper.parseJsonTo(card)))
@@ -112,9 +119,11 @@ public class CashCardControllerUnityTests {
 
     @Test
     void shouldBeUpdatedCashCardIsFailWhenCashCardNotFound() throws Exception {
-        var card = new CashCard(100L,1239.99, "henrique");
+        var card = new CashCard(100L, 1239.99, "henrique");
         given(cashCardRepository.existsById(100L)).willReturn(false);
         mockMvc.perform(put("/api/v1/cashcards/100")
+                        .with(httpBasic("henrique", "1234"))
+                        .with(csrf())
                         .accept(MediaType.APPLICATION_JSON)
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(CashCardTestHelper.parseJsonTo(card)))
@@ -127,6 +136,8 @@ public class CashCardControllerUnityTests {
     @Test
     void shouldBeUpdatedCashCardIsFailWhenRequestBodyIsEmpty() throws Exception {
         mockMvc.perform(put("/api/v1/cashcards/100")
+                        .with(httpBasic("henrique", "1234"))
+                        .with(csrf())
                         .accept(MediaType.APPLICATION_JSON)
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(""))
